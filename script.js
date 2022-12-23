fe576bade420be688e3280e49f34d2396e710540
AniFloyd <anirban7333@gmail.com>
2022-12-23 07:34:39 -0800

    on progress

---
+++ script.js
@@ -0,0 +1,8 @@
+// Type Effect
+var typed3 = new Typed('#type', {
+    strings: ['Market Strategy','Future Planning', 'Start-Up Ideas','Business Planning'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
