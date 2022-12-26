6b690a8207c00dbf064f6a67a71f9e00dfeb2505
AniFloyd <anirban7333@gmail.com>
2022-12-26 03:17:36 -0800

    updated

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ var typed3 = new Typed('#type', {
     smartBackspace: true, // this is a default
     loop: true
   });
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin:500,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    nav: false,
+    dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+  })
